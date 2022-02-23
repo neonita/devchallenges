13af2d3a5c738e9f1384124eb709f6299c4af3d5
neonita <neonita888@gmail.com>
2022-02-22 22:12:38 -0500

    logo and attribution card disappears when hamburger menu is clicked and active

--- Responsive-Web-Developer/interior-consultant-master/js/tablet-mobile-menu.js
+++ Responsive-Web-Developer/interior-consultant-master/js/tablet-mobile-menu.js
@@ -1,13 +1,14 @@
 let hamburger = document.querySelector(".hamburger");
 let navList = document.querySelector(".nav__list");
-let card = document.querySelector(".grid-right__card--active");
+let card = document.querySelector(".grid-right__card");
+let logo = document.querySelector(".nav__logo");
 
 // menu open
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navList.classList.toggle("active");
-
-  card.style.display = "none";
+  card.classList.toggle("grid-right__card--remove");
+  logo.classList.toggle("nav__logo--remove");
 });
 
 let navLink = document.querySelectorAll(".nav__link");
@@ -17,7 +18,7 @@ navLink.forEach((n) =>
   n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navList.classList.remove("active");
-
-    card.style.display = "block";
+    card.classList.remove("grid-right__card--remove");
+    logo.classList.remove("nav__logo--remove");
   })
 );
