3b7dff068b5c8c6f25122056c51e99e1759862a3
neonita <neonita888@gmail.com>
2022-03-02 15:06:34 -0500

    list checkboxes instead of placing them in divs

--- Responsive-Web-Developer/recipe-page-master/js/script.js
+++ Responsive-Web-Developer/recipe-page-master/js/script.js
@@ -1,6 +1,8 @@
-let order = document.querySelector(".instructions__step");
-let value = [1, 2, 3, 4, 5, 6, 7];
+// let order = document.querySelector(".instructions__step");
+// let value = [1, 2, 3, 4, 5, 6, 7];
 
-for (let i = 0; i < value.length; i++) {
-  order.innerHTML = `${i}`;
-}
+// for (let i = 0; i < value.length; i++) {
+//   order.innerHTML = `${i}`;
+// }
+
+// https://www.youtube.com/watch?v=SXb5LN_opbA
